c698b11a24181b47d1f1d3f3589d1d6c4a418507
marramarco.matthieu@gmail.com <marramarco.matthieu@gmail.com>
2021-11-26 18:06:02 +0100

    test with modified startup

--- Startup.cs
+++ Startup.cs
@@ -36,6 +36,7 @@ namespace GladiatorBeastGuide
             }
 
             app.UseHttpsRedirection();
+            app.UseDefaultFiles();
             app.UseStaticFiles();
 
             app.UseRouting();
